3de5ca332b294898ca7c33b608dca742eacca219
max <maximilian.nasarewski@googlemail.com>
2013-02-03 20:43:35 +0000

    include code from other files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 function _(x){
-	return documen.getElementById(x);
+	return document.getElementById(x);
 }
+function ResourceList(a){
+
+}
+function renderPagelet(a){
+	console.log(a);
+}
+
+//on page BEGIN
+ResourceList({
+	"jquery":"//ajax.googleapi.com/",
+	"main":""
+});
+renderPagelet({
+	id:1
+	resources:["jquery","main"],
+	html:'<a href=""></a>'
+});
+//on page END
